706f2d4f78aab7ba1f5242d8166f23659ace71bd
서정희 <sjhlko@naver.com>
2022-12-01 03:13:26 +0900

    4번 과제 해결

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,18 @@
 package onboarding;
 
 public class Problem4 {
+    public static char changedCharacter(char c){
+        if(c>='a'&&c<='z')
+            return (char) ('a'+'z'-c);
+        if(c>='A'&&c<='Z')
+            return (char) ('A'+'Z'-c);
+        return c;
+    }
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            answer.append(changedCharacter(word.charAt(i)));
+        }
+        return answer.toString();
     }
 }
