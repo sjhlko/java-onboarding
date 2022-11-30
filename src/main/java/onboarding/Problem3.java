add33cc38e1a744238628afb35fb3357be364ba0
서정희 <sjhlko@naver.com>
2022-12-01 02:49:51 +0900

    3번 과제 해결

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,21 @@
 package onboarding;
 
 public class Problem3 {
+    public static int checkClap(int number){
+        int clapNumber = 0;
+        while(number!=0){
+            int NthDigit = number%10;
+            if(NthDigit==3||NthDigit==6||NthDigit==9)
+                clapNumber++;
+            number/=10;
+        }
+        return clapNumber;
+    }
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer+=checkClap(i);
+        }
         return answer;
     }
 }
